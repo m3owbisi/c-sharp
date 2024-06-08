50a88e9e79ceaba9a19d2ea891df682b4167cd53
m3owbisi <146631342+m3owbisi@users.noreply.github.com>
2024-06-08 17:45:27 +0530

    Add files via upload

---
+++ 1. write your first code using c#/write-your-first-c#-code.cs
@@ -0,0 +1,14 @@
+Console.WriteLine("Hello World!");
+// console.WriteLine("Hello World!");
+// Console.WriteLine('Hello World!');
+// Console.WriteLine("Hello World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");
+Console.WriteLine("Hello World!");
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
